4d40ede36aae794739653c026cf11abb10d3a033
Portalyst <steevex1977@gmail.com>
2024-05-05 15:12:47 +0500

    Update barrel.gd

--- game/items/barrel.gd
+++ game/items/barrel.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 var speed = 1
 var sl = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	if sl == true:
 		#if Input.is_action_just_pressed("grab"):
 		var direction = get_global_mouse_position() - global_position
@@ -13,6 +16,9 @@ func _physics_process(delta):
 			if sl == false:
 				velocity.x = move_toward(velocity.x, 0, speed)
 				velocity.y = move_toward(velocity.y, 0, speed)
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.y = move_toward(velocity.y, 0, speed)
 	move_and_slide()
 
 #func _on_button_mouse_entered():
